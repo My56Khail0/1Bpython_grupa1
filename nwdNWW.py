c72ff0d87796da385cf0b01ed029deb91301c839
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-10-20 14:01:12 +0200

    Create nwdNWW.py
    oblicza nww
    nwd

---
+++ nwdNWW.py
@@ -0,0 +1,37 @@
+# a = int(input("'')
+a = 15
+b = 21
+# tworzymy dwie dodatkwe zmienne do obliczen nww
+c = a
+d =b 
+
+# print(a,b)
+# c = a//dodatkowa zmienna by zmienić 
+# a = b 
+# b = c
+#zmiana  wartości a z b
+# a,b = b,a 
+# print(a,b)
+#nawjwiększy wspólny dzielnik dwóch podanych liczb
+# while a!=b:
+#   if a>b:
+#     a = a-b
+#   else:
+#     b=b-a
+    
+# print(a)
+
+while b!=0:
+  a,b = b, a%b
+
+print(a)
+
+#największy wspólny dzielnik
+# nww = a*b /nwd
+
+nww = c*d / a
+print(int(nww))
+
+
+
+  
