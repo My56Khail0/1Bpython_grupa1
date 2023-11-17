1c216ea2824c19d3751ffef260c46afd18ce5535
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-17 14:17:07 +0100

    zagadki
    program temperatury
    program czy nadajesz się do szkoły
    sprawdza czy pies > kot

---
+++ ZLekcji/zagadki17_11_2023.py
@@ -0,0 +1,72 @@
+import random
+# x = int(input("liczba "))
+# print(type(x))#napisze type - zwraca typ
+# z = float(input()) #zmiennoprzecinkowa np. 3.14
+# print(type(z))
+# y = input("słowo ")
+# #print(type(y))
+
+# if y == 5:
+#   print("to wypisuję na ekranie")
+
+# temperatura = float(input("Podaj temperaturę "))
+
+# if temperatura >= 30:
+#   print("ty jeszcze tu , przy 30 w KURTCE")
+# if 20 > temperatura > 30:
+#   print("idź se ")
+# if 10 >temperatura >=20:
+#   print("idź")
+# if 0 >temperatura >= 10:
+#    print("idź jak chcesz")
+# else:
+#   print("ubierz czapkę ")
+
+# if temperatura >= 30:
+#   print("ty jeszcze tu , przy 30 w KURTCE")
+# elif temperatura >= 20:
+#   print("idź se ")
+# elif temperatura >=10:
+#   print("idź")
+# elif temperatura >= 0:
+#    print("idź jak chcesz")
+# else:
+#   print("ubierz czapkę ")
+
+# if 4 != "4":
+#   print("...")
+# lista = ["kot", "pies", "freddy fuzbear"]
+# lista.sort()
+# print(lista)
+
+#sortuje alfabetycznie
+# if "kot" < "pies" and "kot" < "kociaczek":
+#   print("prawda")
+# else:
+#   print("fałsz")
+# wiek = int(input("podaj swój wiek: "))
+# if 18 <= wiek <= 30:
+#   matura = int(input("Ile % na maturze "))
+#   if matura >= 75:
+#     print("Możesz iść na studia")
+#   else:
+#     print("sory nie możesz iść dalej jesteś za ..")
+# else:
+#   print("masz za mało lat, idź oglądać ninja go")
+
+c = "\\noga\\tupet"  #co się wyświetli
+'''
+to jest
+komentarz
+na wiele
+linijek
+'''
+c = '''
+to drukuje       tak   jak   napisane'''
+# print(c)
+## to do pokra 
+karta = random.randint(2, 11)
+karta2 = random.randint(2, 11)
+print(f"karta1 {karta} karta2 {karta2} \nsuma = {karta + karta2}")
+# print("karta",karta,"karta2", karta2)
+print("QUIZ")
