1e2d0e225de7226c18a7d504c874a2b8b33bd9a9
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-24 13:51:45 +0100

    Update activity1.py

--- Zadania/activity1.py
+++ Zadania/activity1.py
@@ -3,17 +3,24 @@
 # Zadanie 2  Ile do końca lekcji? Wczytaj od użytkownika ile minęło już minut lekcji i wyświetl ile zostało do końca lekcji. 
 
 #Zadanie 3 Jesteś rektorem uczelni i chcesz sprawdzić Wczytaj wiek od użytkownika - jeśli wiek jest w przedziale [18,40]  
+to wypisz "Możesz iść na studia" w przeciwnym razie "Zapraszamy na kursy i szkolenia"
 
-# Zadanie 4 Oblicz ile wody potrzeba do napełnienia wanny. Pobierz od użytkownika wymiary wanny długość,szerokość, wysokość. Pobierz wartości w cm na końcu podziel przez 1 000 000, aby uzyskać wynik w metrach sześciennych., podziel przez 1000 by uzyskać wynik w litrach - wyświetl odpoiweni komunikat dla użytkownika 
+# Zadanie 4 Oblicz ile wody potrzeba do napełnienia wanny. Pobierz od użytkownika wymiary wanny długość,szerokość, wysokość. 
+Pobierz wartości w cm na końcu podziel przez 1 000 000, aby uzyskać wynik w metrach sześciennych., 
+podziel przez 1000 by uzyskać wynik w litrach - wyświetl odpoiweni komunikat dla użytkownika 
 
 # Zadanie 5  Z jaką prędkością jedzie auto? Wczytaj dystans w km i czas w godz, a następnie podaj wynik “auto jedzie ..km/h” 
 
 # Zadanie 6 Pilny uczeń ma trzy oceny - pobierz je od niego i oblicz jaką będzie miał średnią - zaokrąglij wynik do 2 miejsc po przecinku 
 
-# Zadanie 7 Wersja hard: Pilny uczeń chce wiedzieć jaką będzie miał ocenę na koniec roku. Wszystkie oceny mają wagę 1. Zapytaj się ile ma ocen, a następnie pobierz każdą ocenę i oblicz średnią 
+# Zadanie 7 Wersja hard: Pilny uczeń chce wiedzieć jaką będzie miał ocenę na koniec roku. Wszystkie oceny mają wagę 1. Zapytaj się ile ma ocen, 
+a następnie pobierz każdą ocenę i oblicz średnią 
 
-#Zadanie 8 Program, który sprawdzi czy podana liczba jest podzielna przez 2 i przez 3. Jeśli tak to program wypisze, że "liczba jest podzielna przez " jeśli nie to wypisze "liczba" 
+#Zadanie 8 Program, który sprawdzi czy podana liczba jest podzielna przez 2 i przez 3. Jeśli tak to program wypisze, że "liczba 
+jest podzielna przez " jeśli nie to wypisze "liczba" 
 
-#Zadanie 9 Program, który sprawdzi jaka jest ostatnia cyfra  podanej liczby dwucyfrowej, jeśli to będzie 5 lub 0 to wypisze "liczba podzielna przez 5"  jeśli nie to wypisze "liczba" 
+#Zadanie 9 Program, który sprawdzi jaka jest ostatnia cyfra  podanej liczby dwucyfrowej, jeśli to będzie 5 lub 0 to wypisze "liczba podzielna przez 5"  
+jeśli nie to wypisze "liczba" 
 
-#Zadanie 10 Program, który wczyta dwie liczby i sprawdzi czy podana liczba jest jest podzielna przez drugą podaną liczbę przez użytkownika. jeśli tak to wypisze "liczba podzielna przez liczbę" jeśli nie to wypisze "liczba niepodzielna przez liczbę" 
+#Zadanie 10 Program, który wczyta dwie liczby i sprawdzi czy podana liczba jest jest podzielna przez drugą podaną liczbę przez użytkownika. jeśli tak to 
+wypisze "liczba podzielna przez liczbę" jeśli nie to wypisze "liczba niepodzielna przez liczbę" 
