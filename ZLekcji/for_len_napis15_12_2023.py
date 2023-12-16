c6dd33045fd58a91bfee8c1173a2d54af02e48ed
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-12-16 09:10:43 +0100

    Create for_len_napis15_12_2023.py

---
+++ ZLekcji/for_len_napis15_12_2023.py
@@ -0,0 +1,90 @@
+# napis = "You shall not pass"
+
+# z = int(input("podaj coś "))
+# print(type(z))
+# z = float(input("podaj coś "))
+# print(type(z))
+# z = input("podaj coś ")
+# print(type(z))
+
+# print(chr(0))
+# print(ord("a"))
+
+
+# napis ="kajak1"
+# a = len(napis)# oblicza długość
+# if napis == napis[::-1]:
+#   print("palindrom") 
+# else:
+#   print("nie jest palindromem")
+
+# print(napis[:10])#napis[0:10]
+# print(napis[10:])#napis[10:len(napis)]
+
+# for a in range(2,20,2):
+#   print(a)
+
+# if "kot1" >  "kot":
+#   print("prawda")
+# else:
+#   print("nieprawda")
+
+# print(a)
+# for i in range(len(napis)):
+#   print(i)
+
+# print(a)
+# for i in napis:
+#   print(i)
+
+# for i in range(1, 101,2):
+#   print(i,end=" ")
+
+# print(1,2,3,4,sep="%") #separator
+# a = 3
+# b = 2
+# if a == 1 and b == 2: #i - wszystkie spełnione 
+#   print(a)
+# else:
+#   print(a>0) #True 
+
+# if a == 1 or "pies" > "kot": #
+#   print("prawda")
+# else:
+#   print("fałsz")
+
+# 5//2 = 2
+# 5%2 = 1#modulo -reszta z dzielenia
+# 10**3 = 1000
+
+# running = True
+# licznik = 0
+# while running:
+#   print(licznik+1, "prawda")
+#   licznik +=1
+#   if licznik ==10:
+#     break
+
+
+
+
+
+# while 
+# for
+# odp = "tak"
+# while odp=="tak":
+#   odp = input("czy chcesz kontunuować?").lower()
+
+
+
+
+# print(napis.lower())
+# print(napis.upper())
+
+# tired = True
+# while tired:
+#   print("go to bed")
+#   question =input("Are you tired? ").upper()
+#   if question != "YES":
+#     #tired = False
+#     break
