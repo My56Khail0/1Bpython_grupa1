33c982c5f4cad90df1d5991b5092c4d8234de7a3
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2024-01-11 21:08:39 +0100

    Create noworoczny_sprawdzianik.py
    help to summarize the knowledge about basic concepts in programming

---
+++ Zadania/noworoczny_sprawdzianik.py
@@ -0,0 +1,24 @@
+# Pobierz z ekranu swoje imię i wyświetl napis- “Twoje imię” - super, że jesteś
+# Właśnie zakładasz stronę z treściami tylko dla dorosłych. Wyświetl na ekranie stosowny komunikat i spytaj się użytkownika o wiek - jeśli ma poniżej 18, w grzeczny sposób odmów dostępu do strony, w przeciwnym razie napisz, że użytkownik może korzystać ze strony.
+# Oblicz BMI - dla podanych przez użytkownika wartości wagi i wzrostu -należy podzielić wagę wyrażoną w kilogramach przez wzrost podniesiony do kwadratu, wyrażony w metrach, czyli w skrócie BMI = kg / m2.
+# Zdrowy dorosły człowiek o prawidłowym stosunku wagi do wzrostu ma wskaźnik w granicach 18,5–24,9. BMI poniżej 18,5 informuje o niedowadze. Osoby ze wskaźnikiem BMI 25,0-29,9 cierpią na nadwagę. Współczynnik BMI powyżej 30,0 to już otyłość.Wyświetl odpowiedni komunikat do uzyskanego wyniku. 
+# Wyświetl na ekranie liczby nieparzyste od 10 do 10 (włącznie) <10,20> w pętli for lub while
+# Jesteś rekruterem stewardess do linii lotniczych. Chcesz by wszystkie dziewczyny w Twoim zespole miały wzrost od <167-180> . Sprawdź czy podane dane przez użytkownika spełniają ten warunek.
+
+# Wyświetl na ekranie liczby parzyste od 0 do 10 (włącznie) <0,10> w pętli for lub while
+# Jesteś geografem i chcesz przy pomocy komputera w szybki sposób policzyć jaka jest różnica między najwyższym a najniższym miejscem w podanym kraj. Najpierw policz jaka jest różnica w Polsce. depresja Marzęcino -2.2 Rysy 2499. Użytkownik podaje dwie liczby. Dla ułatwienia obliczeń najpierw podaje liczbę mniejszą(bądź też ujemną)
+
+
+# Oblicz wartość bezwzględną z podanej przez użytkownika liczby
+# jak Ci do śmiechu. na ekranie pojawia się tyle razy napis "Ha" ile razy wskazuje na to liczba podana przez użytkownika
+# oblicz swoją średnią ocen z Informatyki? Czy, wg Ciebie, zasługujesz na dobrą ocenę? Obliczenia dokonaj w pętli najpierw podają liczbę ocen a potem po kolei pytając się o kolejną ocenę. zadanie 1. oblicz średnią arytmetyczną przyjmując, że oceny nie mają wag - np. 1,2,3 -> 2.0. 2. oblicz średnią ważoną - wynik zaokrąglić do 2 miejsc po przecinku (pytają się w pętli za każdym razem o wagę) , 1 w.3, 2 w.2, 3 w.1 -> 1.67
+# Jesteś sędzią zawodów, właśnie odliczasz czas od 10 do 1. Wyświetl te liczby na ekranie za każdym razem z odpowiednią liczbą kropek
+# 10 ……….
+# 9  ………
+# 8  ……..
+# itd….
+# Oblicz pole kwadratu dla podanej przez użytkownika długości boku (liczba całkowita).(dane 9 wynik 81)
+# Oblicz pole koła dla podanego przez użytkownika długości promienia (dane 6, wynik po zaokrągleniu 37.7)
+# Wczytaj długości boku prostokąta i oblicz pole (dane 4, 5 wynik 20)
+# Wczytaj długości boku prostokąta i oblicz obwód (dane 4,5 wynik 18))
+
